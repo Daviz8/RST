4009cb3afd5c9386a1e575fc1673f2583ac004d4
Daviz8 <okorowhyme234@gmail.com>
2025-06-13 14:30:55 +0100

    folder restructuring , added readme.md

--- server/src/config/swagger.ts
+++ server/src/config/swagger.ts
@@ -1,9 +1,5 @@
 import swaggerJsdoc from "swagger-jsdoc";
 
 export const swaggerSpec = swaggerJsdoc({
-  definition: {
-    openapi: "3.0.0",
-    info: { title: "Chat API", version: "1.0.0" },
-  },
-  apis: ["./src/routes/*.ts"],
+ 
 });
